0b8dbb503d153148330eddd6b56181ecf2bd5a65
Nirbhay Kumar <niravkr5854@gmail.com>
2026-09-03 21:20:06 +0530

    Update server.js

--- session6-7-docker/Docker Fundamental HW/nodejs-app/server.js
+++ session6-7-docker/Docker Fundamental HW/nodejs-app/server.js
@@ -1,4 +1,3 @@
-cat > server.js <<'EOF'
 const http = require('http');
 
 const hostname = '0.0.0.0';
@@ -7,10 +6,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+
   res.end('<h1>Hello World from Node.js!</h1>');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-EOF
